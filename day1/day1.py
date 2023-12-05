596fe41222f6bf2b34677183e45c8db5606383a7
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-04 22:42:08 -0800

    added scrolling window implementation solution to part 2

--- day1/day1.py
+++ day1/day1.py
@@ -28,7 +28,7 @@ def part1(filename, test=False):
 
 # Part 2
 # TODO: simple find and replace won't work. Use a sliding window to match for the first word greedily (iterate over from longest word to the shortest starting with left pointer at 0 and going until left pointer is > length / 2. Then do the same in reverse from the right) and then replace THAT!
-map = {
+dictionary = {
     "one": "1",
     "two": "2",
     "six": "6",
@@ -42,43 +42,49 @@ map = {
 def part2(filename, test=False):
     total = 0
     for string in open(filename, "r").readlines():
-        # replace first occurence of word with number
         if test:
-            print("k->v subst:")
-        for key, value in map.items():
+            print(f"str: {string}")
+        # scroll index across string
+        number = ["", ""]
+        for index in range(0, len(string)):
+            # add char to number if it is a digit
+            if string[index].isdigit():
+                if test:
+                    print(f"adding digit {string[index]}")
+                if number[0] == "":
+                    number[0] = string[index]
+                else:
+                    number[1] = string[index]
+            # otherwise check if it's a recognized word
+            else:
+                if test:
+                    print("checking against dictionary")
+                # iterate over dictionary
+                for word, digit in dictionary.items():
+                    # check all characters match in both strings
+                    if test:
+                        print(f"word: {word}")
+                    is_number = True
+                    for i in range(0, len(word)):
+                        if test:
+                            print(f"i: {i}, index: {index}\n str: {string[i + index]}, word: {word[i]}")
+                        if string[i + index] != word[i]:
+                            is_number = False
+                    # if all characters match
+                    if is_number:
+                        # add digit
+                        if test:
+                            print(f"adding digit: {digit}")
+                        if number[0] == "":
+                            number[0] = digit
+                        else:
+                            number[1] = digit
+                        break
             if test:
-                print(string)
-            if string.find(key) != -1:
-                string = string.replace(key, value, 1)
-                break
-        # replace last occurence of word with number (sdrawkcab)
-        for key, value in map.items():
-            if test:
-                print(string)
-            if string[::-1].find(key[::-1]) != -1:
-                string = string[::-1].replace(key[::-1], value[::-1], 1)
-                string = string[::-1]
-                break
-        # then continue with normal part1 solution
-        if test:
-            print(string)
-            print("index | character")
-        number = ""
-        for l in range(0, len(string)):
-            if test:
-                print(l, string[l])
-            if string[l].isdigit():
-                number += string[l]
-                break
-        for r in range(len(string) - 1, -1, -1):
-            if test:
-                print(r, string[r])
-            if string[r].isdigit():
-                number += string[r]
-                break
-        if test:
-            print("num:", number)
-        total += int(number)
+                print(f"i: {index}, ch: {string[index]}, num: {number}")
+        if number[1] == "":
+            number[1] = number[0]
+        total += int("".join(number))
         if test:
             print("total:", total)
     return total
