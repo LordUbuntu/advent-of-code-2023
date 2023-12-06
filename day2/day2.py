c3972d67b68bd4e26f6186947a93dccf121bd623
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-05 21:17:26 -0800

    wrote solution for day 2 part 2

--- day2/day2.py
+++ day2/day2.py
@@ -2,7 +2,8 @@
 
 
 # Part 1
-# The hardest part is reading the input lol
+# Count ID numbers of all the games which are possible (don't exceed maximum value
+#   for any color at any one time)
 def part1(filename):
     total = 0
     for game in open(filename, "r").readlines():
@@ -32,6 +33,31 @@ def part1(filename):
     return total
 
 
+# Part 2
+# Find the maximum occuring value for each color, then add their power to total
+from functools import reduce
+from operator import mul
+def part2(filename):
+    total = 0
+    for game in open(filename, "r").readlines():
+        # parse game
+        id = int(game.split()[1].strip(':'))  # game ID number
+        counts = game.split()[2:]  # number of each color
+        counts = [count.strip(" ,;:") for count in counts]  # sanitize
+
+        # keep track of maximum color occuring (minimum needed to be possible)
+        rgb_max = {"red": 0, "green": 0, "blue": 0}
+        for i in range(0, len(counts) - 1, 2):
+            count, color = counts[i : i + 2]
+            rgb_max[color] = max(rgb_max[color], int(count))
+
+        # add power to total
+        total += reduce(mul, rgb_max.values())
+        print(game, counts, id, rgb_max, total)
+    return total
+
+
 if __name__ == "__main__":
     print(part1("input/test1.txt"))
     print(part1("input/part1.txt"))
+    print(part2("input/test2.txt"))
