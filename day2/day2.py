12ffdee606bd181c8d5ce20fec9cf09779c96fec
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-05 20:47:54 -0800

    robusted input processing

--- day2/day2.py
+++ day2/day2.py
@@ -2,29 +2,30 @@
 
 
 # Part 1
+# The hardest part is reading the input lol
 def part1(filename):
     total = 0
     for game in open(filename, "r").readlines():
-        id = int(game.split()[1].strip(':'))  # get game ID number
+        id = int(game.split()[1].strip(':'))  # game ID number
+        counts = game.split()[2:]  # number of each color
+        counts = [count.strip(" ,;:") for count in counts]  # sanitize
         rgb = [0, 0, 0]
         # parse totals
-        for i in range(len(game)):
-            if game[i].isdigit():
-                count = int(game[i])
-                color = game[i + 2]
-                if color == 'r':
-                    rgb[0] += count
-                if color == 'g':
-                    rgb[1] += count
-                if color == 'b':
-                    rgb[2] += count
+        for i in range(0, len(counts) - 1, 2):
+            count, color = counts[i : i + 2]
+            if color == "red":
+                rgb[0] += int(count)
+            if color == "green":
+                rgb[1] += int(count)
+            if color == "blue":
+                rgb[2] += int(count)
         # if game is not possible, continue to next game
         if rgb[0] <= 12 and rgb[1] <= 13 and rgb[2] <= 14:
             total += id
-        # print(game, rgb, id, total)
+        # print(game, id, counts, rgb, total)
     return total
 
 
 if __name__ == "__main__":
     print(part1("input/test1.txt"))
-    print(part1("input/part1.txt"))
+    # print(part1("input/part1.txt"))
