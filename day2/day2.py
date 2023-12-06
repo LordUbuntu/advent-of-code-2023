69eb4aab81da0551cade4019be791aafadbbdee0
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-05 20:57:55 -0800

    corrected faulty logic: the limit is max number of cubes pulled out at any one time, not the total of each

--- day2/day2.py
+++ day2/day2.py
@@ -6,26 +6,32 @@
 def part1(filename):
     total = 0
     for game in open(filename, "r").readlines():
+        # parse game
         id = int(game.split()[1].strip(':'))  # game ID number
         counts = game.split()[2:]  # number of each color
         counts = [count.strip(" ,;:") for count in counts]  # sanitize
-        rgb = [0, 0, 0]
-        # parse totals
+
+        # check for any impossible games
+        possible = True
         for i in range(0, len(counts) - 1, 2):
             count, color = counts[i : i + 2]
-            if color == "red":
-                rgb[0] += int(count)
-            if color == "green":
-                rgb[1] += int(count)
-            if color == "blue":
-                rgb[2] += int(count)
-        # if game is not possible, continue to next game
-        if rgb[0] <= 12 and rgb[1] <= 13 and rgb[2] <= 14:
+            if color == "red" and int(count) > 12:
+                possible = False
+                break
+            if color == "green" and int(count) > 13:
+                possible = False
+                break
+            if color == "blue" and int(count) > 14:
+                possible = False
+                break
+
+        # tally possible games
+        if possible:
             total += id
-        # print(game, id, counts, rgb, total)
+        # print(id, total)
     return total
 
 
 if __name__ == "__main__":
     print(part1("input/test1.txt"))
-    # print(part1("input/part1.txt"))
+    print(part1("input/part1.txt"))
