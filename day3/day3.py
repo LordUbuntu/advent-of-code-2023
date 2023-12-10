15cb4062157b69d13eeabb3dd5b8646f89552306
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-09 20:40:56 -0800

    added main body

--- day3/day3.py
+++ day3/day3.py
@@ -7,3 +7,23 @@ def part1(filename):
     queue = []  # remember digits in order
     width, height = len(schematic[0]), len(schematic)
     part = False
+    print(f"width: {width}, height {height}, schematic: {schematic}")
+    for i in range(width):
+        for j in range(height):
+            print(f"loop\nsymbol: {schematic[i][j]}, i: {i}, j: {j}")
+            # we've reached the end of a sequence
+            if schematic[i][j] == '.':
+                print(f"  queue was {queue}, part {part}")
+                # if we've reached the end of a valid part number
+                if part:
+                    # add the part number to the total
+                    total += int(''.join(queue))
+                    print(f"    total {total}")
+                # reset queue and part flag
+                part = False
+                queue = []
+            # add digits to queue in order (left-to-right)
+            elif schematic[i][j].isdigit():
+                print("  add digit to queue")
+                queue.append(schematic[i][j])
+            # look at all adjacent tiles (3x3 kernel) for part symbols
