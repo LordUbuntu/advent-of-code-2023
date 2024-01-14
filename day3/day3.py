eb89904e9e74cb4244f6003e17658e6d17805988
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-13 19:42:44 -0800

    construct numbers within schematic

--- day3/day3.py
+++ day3/day3.py
@@ -5,10 +5,15 @@ def part1(filename):
     def bound(n, low, high): return max(low, min(high, n))
     total = 0
     schematic = [line.strip() for line in open(filename).readlines()]
+    number = []  # contruct numbers from a queue of digits
     # scan pointer across grid
     for row in range(len(schematic)):
         for col in range(len(schematic[0])):
-            pass
-            # if a digit
-            # if not
+            char = schematic[row][col]
+            if char.isdigit():
+                number.append(char)
+                print("digit: {}, number: {}".format(char, number))
+            else:
+                print(''.join(number))
+                number = []
     return total
