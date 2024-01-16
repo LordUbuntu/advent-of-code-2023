0fa8d8e638d20b8a5227b08aa34e089f16b1ef4c
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-15 20:21:02 -0800

    planning part 2

--- day3/day3.py
+++ day3/day3.py
@@ -40,31 +40,6 @@ def part2(filename):
     total = 0
     schematic = [line.strip() for line in open(filename).readlines()]
     number = []  # contruct numbers from a queue of digits
-    part_number = False
-    # scan pointer across grid
-    for row in range(len(schematic)):
-        for col in range(len(schematic[0])):
-            char = schematic[row][col]
-            # terminate and add part number at end of numeric string
-            if not char.isdigit():
-                # add number to total if it's a part number
-                if part_number:
-                    total += int(''.join(number))
-                # reset digits for next number
-                number = []
-                part_number = False
-            # remember digits of number in order
-            if char.isdigit():
-                number.append(char)
-            # check for adjacent part symbols around current number
-            if len(number) > 0:
-                for i in range(-1, 2):
-                    for j in range(-1, 2):
-                        y, x = row + i, col + j
-                        if y < 0 or y >= len(schematic):
-                            continue
-                        if x < 0 or x >= len(schematic[0]):
-                            continue
-                        if schematic[y][x] != '.' and not schematic[y][x].isdigit():
-                            part_number = True
+    # same as part1 but this time only paying attention to *
+    # when a * is encountered adjacent to a number, find the start of the other number also adjacent to it, get the numeric string of both, and add their values together
     return total
