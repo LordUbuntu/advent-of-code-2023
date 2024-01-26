dcb974282464a89a7a7d65e2346e214175786fe7
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-26 15:46:03 -0800

    began plan for part 1

--- day4/day4.py
+++ day4/day4.py
@@ -1,2 +1,11 @@
 # Jacobus Burger (2023)
 # Advent of Code - Day 4
+
+
+# Part 1
+def part1(filename):
+    # The solution to part 1 is simple. We need to count how many
+    #   elements from the left are members of the right, then raise
+    #   2 to the power of that count to get the number of points.
+    # In python this is a simple matter of set intersection.
+    pass
