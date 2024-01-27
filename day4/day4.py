17ae0ed674390762b8752382e0bdd2e1ca248454
Jacobus Burger <therealjacoburger@gmail.com>
2024-01-26 16:07:43 -0800

    finished solution for day 4 part 1

--- day4/day4.py
+++ day4/day4.py
@@ -7,5 +7,16 @@ def part1(filename):
     # The solution to part 1 is simple. We need to count how many
     #   elements from the left are members of the right, then raise
     #   2 to the power of that count to get the number of points.
-    # In python this is a simple matter of set intersection.
-    pass
+    # In python this is a simple matter of set intersection after
+    #   processing the input.
+    total = 0
+    cards = [line.strip() for line in open(filename).readlines()]
+    for card in cards:
+        # get sets of numbers from boths sides of '|'
+        A, B = card.split('|')
+        A = {int(a) for a in A.split() if a.isdigit()}
+        B = {int(b) for b in B.split() if b.isdigit()}
+        winners = len(A.intersection(B))
+        if winners:
+            total += 2**(len(A.intersection(B)) - 1)
+    return total
